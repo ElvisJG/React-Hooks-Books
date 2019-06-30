ebf901884094263636a17516259ac4f7939712ca
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-06-30 00:48:24 -0400

    more hook practice :

---
+++ hooksapp/src/components/SongList.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import uuid from 'uuid/v1';
+import NewSongForm from './NewSongForm';
+
+export default function SongList() {
+  const [songs, setSongs] = useState([
+    { title: 'Almost Home', id: 1 },
+    { title: 'Memory Gospel', id: 2 },
+    { title: 'This Wild Darkness', id: 3 }
+  ]);
+  const addSong = title => {
+    setSongs([...songs, { title, id: uuid() }]);
+  };
+  return (
+    <div className='song-list'>
+      <ul>
+        {songs.map(song => {
+          return <li key={song.id}>{song.title}</li>;
+        })}
+      </ul>
+      <NewSongForm addSong={addSong} />
+    </div>
+  );
+}
